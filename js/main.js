de279159b9b597201cdfe4ad242a527125df94b6
MinaMedhat86 <karakibo12@gmail.com>
2023-12-11 15:52:02 +0200

    Second Commit

--- js/main.js
+++ js/main.js
@@ -5,8 +5,10 @@ var productName = document.getElementById("productName");
 var productSalary = document.getElementById("productSalary");
 var productCategory = document.getElementById("productCategory");
 var productDesc = document.getElementById("productDesc");
-var addBtn = document.getElementById("addBtn");
-var updateBtn = document.getElementById("updateBtn");
+
+
+var searchWord =  document.getElementById("searchWord");
+
 var indexUpdate;
 var mood = "create";
 
@@ -20,6 +22,9 @@ if(localStorage.getItem("productStorage")!= null){
 
 
 function addProduct(){
+
+    if (validationName()==true && validationSalary()==true  && validationCategory()==true ){
+
         var product = {
             name : productName.value,
             salary : productSalary.value, 
@@ -41,26 +46,25 @@ function addProduct(){
         
         localStorage.setItem("productStorage", JSON.stringify(productArr));
         productTable(productArr);
-
+    }
 }
 
-
 // Add Product In Table
-function productTable (productArr){
+function productTable (productArr , searchItem ){
     var cartona = "";
     for(i=0 ; i<productArr.length ; i++){
         cartona +=`        <tr>
         <td>${i+1}</td>
-        <td>${productArr[i].name}</td>
+        <td>${searchItem? productArr[i].name.replace(searchItem , `<span class="text-danger fw-bolder">${searchItem}</span>`):productArr[i].name}</td>
         <td>${productArr[i].salary}</td>
         <td>${productArr[i].category}</td>
         <td>${productArr[i].description}</td>
         <td><button onclick="updateForm(${i})" class="btn btn-warning btn-sm">Update</button></td>
         <td><button onclick="deleteProduct(${i})" class="btn btn-danger btn-sm">Delete</button></td>
     </tr> `
-    document.getElementById("tableBody").innerHTML = cartona;
+    
     }
-        
+    document.getElementById("tableBody").innerHTML = cartona;   
     clearForm();
 }
 
@@ -104,7 +108,7 @@ function updateForm(indexProduct){
 
 function searchProduct (searchItem){
     var searchArr = [];
-
+    searchItem = searchWord.value;
     for(i=0 ; i<productArr.length ; i++){
 
         if(productArr[i].name.toLowerCase().includes(searchItem.toLowerCase()) == true)
@@ -113,10 +117,110 @@ function searchProduct (searchItem){
         }
     }
 
-productTable(searchArr);
+productTable(searchArr , searchItem );
     
 }
 
+
+
+                // -------------  Validation ------------ 
+
+// Name
+function validationName(){
+    var regexMessegeName = document.getElementById("regexMessegeName");
+    var regexName = /^[A-Z][a-z]{2,8}$/
+
+    if (regexName.test(productName.value) == true){
+        productName.classList.add("is-valid");
+        productName.classList.remove("is-invalid");
+        regexMessegeName.classList.replace("d-block" , "d-none" );
+
+        return true;
+
+    } else{
+        productName.classList.add("is-invalid");
+        productName.classList.remove("is-valid");
+        regexMessegeName.classList.replace("d-none" , "d-block");
+
+        return false;
+    }
+}
+
+// Salary
+function validationSalary(){
+    var regexMessegeSalary = document.getElementById("regexMessegeSalary");
+    var regexSalary = /^[1-9][0-9]{2,8}$/
+
+    if (regexSalary.test(productSalary.value) == true){
+        productSalary.classList.add("is-valid");
+        productSalary.classList.remove("is-invalid");
+        regexMessegeSalary.classList.replace("d-block" , "d-none" );
+
+        return true;
+
+    } else{
+        productSalary.classList.add("is-invalid");
+        productSalary.classList.remove("is-valid");
+        regexMessegeSalary.classList.replace("d-none" , "d-block");
+
+        return false;
+    }
+}
+
+
+// Category
+function validationCategory(){
+    var regexMessegeCategory = document.getElementById("regexMessegeCategory");
+    var regexCategory = /^(Mobile|Tv|ElectricProduct|else)$/
+
+    if (regexCategory.test(productCategory.value) == true){
+        productCategory.classList.add("is-valid");
+        productCategory.classList.remove("is-invalid");
+        regexMessegeCategory.classList.replace("d-block" , "d-none" );
+
+        return true;
+
+    } else{
+        productCategory.classList.add("is-invalid");
+        productCategory.classList.remove("is-valid");
+        regexMessegeCategory.classList.replace("d-none" , "d-block");
+
+        return false;
+    }
+}
+
+
 // End CRUD System Input Data
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
